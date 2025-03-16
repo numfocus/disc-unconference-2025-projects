a71c57a667ab8eb88524ee9cb0fedc606344a13f
Oriol (VANT Edge) <oriol.abril.pla@gmail.com>
2025-03-16 11:59:54 -0300

    fix links to tag pages

--- unconference/conf.py
+++ unconference/conf.py
@@ -17,6 +17,7 @@ extensions = [
     "sphinx_jupyterbook_latex",
     "sphinx_tags",
 ]
+suppress_warnings = ["etoc.toctree"]
 #language = os.environ.get("WEBSITE_LANGUAGE", "en")
 #locale_dirs = ["../locales"]
 gettext_uuid = True
@@ -80,4 +81,5 @@ suppress_warnings = ["myst.domains"]
 use_jupyterbook_latex = True
 use_multitoc_numbering = True
 tags_create_tags = True
+tags_extension = ["md"]
 tags_create_badges = False
